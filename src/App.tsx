9d747382dcab8490f69140b80d1f4ea83db59459
GEND1T <akunumum1606@gmail.com>
2026-04-29 19:57:14 +0700

    Remove unused Footer import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-
-import Footer from "./components/Footer";
 import Copetition from "./pages/Competition";
 import HomePage from "./pages/HomePage_backup"
 import LoginForm from "./pages/LoginForm";
